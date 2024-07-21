e0fe46f8034d9164197cbca4f100d382d98dee59
nayeemantu14 <159104425+nayeemantu14@users.noreply.github.com>
2024-07-21 14:04:31 +1000

    version 0.2

--- App/app_main.c
+++ App/app_main.c
@@ -12,13 +12,13 @@ extern TIM_HandleTypeDef htim2;      // Declare Timer 2 handler
 extern UART_HandleTypeDef huart2;    // Declare UART handler
 extern RTC_HandleTypeDef hrtc;       // Declare RTC handler
 extern TIM_HandleTypeDef htim21;     // Declare Timer 21 handler
+RTC_AlarmTypeDef sAlarm;             // Declare RTC Alarm structure
 
 // Global variable declaration
 char message[40];                     // Buffer to store messages
 
 uint8_t wupFlag = 1;                  // Initialize wake-up flag
-uint8_t rtcwupFlag = 0;               // Initialize RTC wake-up flag
-volatile static uint8_t mbatt_counter; // Initialize mbatt_counter flag
+uint8_t rtcFlag = 0;				  // Initialize wake-up flag
 static uint8_t Low_battery;           // Initialize low battery flag
 
 volatile static uint8_t valve_open;   // Initialize valve open flag
@@ -30,7 +30,6 @@ volatile static uint32_t pressDuration = 0; // Initialize button press duration
 
 static uint32_t alert_time = 0;       // Initialize alert time
 static uint32_t sleep_time = 0;       // Initialize sleep time
-static uint32_t TimeStamp = 0;        // Initialize timestamp
 
 // Function prototypes
 void openValve(void);                 // Function prototype for opening the valve
@@ -40,14 +39,16 @@ void resetFloodEvent(void);           // Function prototype for resetting the fl
 uint16_t measureBattery(void);        // Function prototype for measuring battery voltage
 void monitorBattery(void);            // Function prototype for monitoring battery voltage
 void statusled(void);                 // Function prototype for system status LED
-void batteryled(void);                // Function prototype for activating battery LED
+void RTC_AlarmConfig(void);
+void batteryled(void);				  // Function prototype for activating battery LED
+void batteryAlarm(void);              // Function prototype for activating battery Alarm
 void console(char *log);              // Function prototype for transmitting messages via UART
 
 // Main application function
 void app_main(void)
 {
     // Initialize message buffer with default message
-    strcpy(message, "EFloodGuardLP(v3.2)\r\n");
+    strcpy(message, "EFloodGuardLP(v3.3)\r\n");
     // Send initialization message
     console(message);
 
@@ -111,22 +112,15 @@ void app_main(void)
                 console(message);
             }
         }
-
-        if(rtcwupFlag)
+        if(rtcFlag)
         {
-            strcpy(message, "RTC event\r\n");
-            console(message);
-            rtcwupFlag = 0;
+        	strcpy(message, "RTC Event\r\n");
+        	console(message);
+        	rtcFlag = 0;
         }
-
         if((now - sleep_time >= 5000) && !floodFlag && wupFlag)
         {
-            statusled();
             monitorBattery();
-            if (mbatt_counter == 59)
-            {
-                monitorBattery();
-            }
             wupFlag = 0;
             strcpy(message, "Entering Sleep\r\n");
             console(message);
@@ -145,7 +139,6 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     HAL_ResumeTick();
     sleep_time = HAL_GetTick();
     wupFlag = 1;
-    mbatt_counter = 59;
     if(GPIO_Pin == GPIO_PIN_15)
     {
         if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15) == GPIO_PIN_SET) // Rising edge
@@ -178,13 +171,8 @@ void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc)
     SystemClock_Config();
     HAL_ResumeTick();
     sleep_time = HAL_GetTick();
-    rtcwupFlag = 1;
     wupFlag = 1;
-    mbatt_counter++;
-    if(mbatt_counter > 59)
-    {
-        mbatt_counter = 0;
-    }
+    rtcFlag = 1;
 }
 
 // Callback function for TIM21 period elapsed interrupt
@@ -265,13 +253,17 @@ uint16_t measureBattery(void)
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET); // Disable battery voltage measurement
 
     // Check battery voltage threshold
-    if(analogbatt < 2950)
+    if(analogbatt < 2950 && analogbatt >= 2800)
     {
         Low_battery = 1; // Set low battery flag if voltage is below threshold
     }
+    else if(analogbatt < 2800)
+    {
+        Low_battery = 2; // Set low battery flag flag if voltage is below critical threshold
+    }
     else
     {
-        Low_battery = 0; // Reset low battery flag if voltage is above threshold
+    	Low_battery = 0;
     }
     return analogbatt; // Return battery voltage reading
 }
@@ -279,10 +271,17 @@ uint16_t measureBattery(void)
 // Function to monitor battery voltage
 void monitorBattery(void)
 {
-    uint16_t vBatt = measureBattery(); // Measure battery voltage
-    if(Low_battery)
+	uint16_t vBatt = measureBattery(); // Measure battery voltage
+    if(Low_battery == 1)
     {
-        batteryled(); // Activate battery LED if low battery
+    	batteryled();
+    	RTC_AlarmConfig();
+    }
+    else if(Low_battery == 2)
+    {
+    	RTC_AlarmConfig();
+    	closeValve();	// Close Valve if Critically low Battery
+    	batteryAlarm(); // Activate battery Alarm if critically low battery
     }
     sprintf(message, "Battery Voltage: %d\r\n", vBatt); // Format battery voltage message
     console(message); // Send battery voltage message via UART
@@ -298,8 +297,16 @@ void statusled(void)
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET); // Deactivate status LED
 }
 
-// Function to activate battery LED
+// Function to activate battery led
 void batteryled(void)
+{
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET); // Activate battery LED
+	HAL_Delay(200); // Delay for LED indication
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET); // Deactivate battery LED
+}
+
+// Function to activate battery Alarm
+void batteryAlarm(void)
 {
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET); // Activate battery LED
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET); // Activate battery LED
@@ -308,6 +315,27 @@ void batteryled(void)
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET); // Deactivate battery LED
 }
 
+void RTC_AlarmConfig(void)
+{
+	HAL_RTC_DeactivateAlarm(&hrtc, RTC_ALARM_A);
+	sAlarm.AlarmTime.Hours = 0x0;
+	sAlarm.AlarmTime.Minutes = 0x0;
+	sAlarm.AlarmTime.Seconds = 0x0;
+	sAlarm.AlarmTime.SubSeconds = 0x0;
+	sAlarm.AlarmTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+	sAlarm.AlarmTime.StoreOperation = RTC_STOREOPERATION_RESET;
+	sAlarm.AlarmMask = RTC_ALARMMASK_DATEWEEKDAY|RTC_ALARMMASK_HOURS
+			|RTC_ALARMMASK_MINUTES;
+	sAlarm.AlarmSubSecondMask = RTC_ALARMSUBSECONDMASK_ALL;
+	sAlarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_DATE;
+	sAlarm.AlarmDateWeekDay = 0x1;
+	sAlarm.Alarm = RTC_ALARM_A;
+	if (HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BCD) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+}
 // Function to activate buzzer and warning LED
 void alert(void)
 {
